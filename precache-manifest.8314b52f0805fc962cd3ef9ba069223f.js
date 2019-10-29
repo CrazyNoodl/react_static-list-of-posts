3c04f71a783ca0a31d70f660e81c04c4cd728b39
Rudenko Artem <artem.rud@bk.ru>
2019-10-29 04:15:31 +0200

    Updates

---
+++ precache-manifest.8314b52f0805fc962cd3ef9ba069223f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e683049315b83723813ac596b033291",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "4120829daabf9a9d3e95",
+    "url": "/react_static-list-of-posts/static/css/main.228bc07d.chunk.css"
+  },
+  {
+    "revision": "4f3fb121ce3d95cec5e6",
+    "url": "/react_static-list-of-posts/static/js/2.463f5126.chunk.js"
+  },
+  {
+    "revision": "4120829daabf9a9d3e95",
+    "url": "/react_static-list-of-posts/static/js/main.ebb91991.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
